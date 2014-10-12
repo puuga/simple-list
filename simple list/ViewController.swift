88d508db63f30afe59990d8bc8ed65079672a00b
puuga <siwaoh@gmail.com>
2014-10-12 10:13:41 +0700

    Initial Commit

---
+++ simple list/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simple list
+//
+//  Created by Siwawes Wongcharoen on 10/12/2557 BE.
+//  Copyright (c) 2557 puuga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
